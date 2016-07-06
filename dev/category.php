5cad3f4505769b10c121873ee00aa948b78783c5
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-07-06 13:23:33 +0100

    db connection test

---
+++ dev/category.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sommy B
+ * Date: 7/6/2016
+ * Time: 1:09 PM
+ */
+
--- dev/test.php
+++ dev/test.php
@@ -13,18 +13,49 @@ include 'assets/functions/functions.php' ?>
 <head>
     <meta charset="UTF-8">
     <title>Arduino Booking System</title>
-
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
+
+
 <body>
 
-<h2>Arduino Booking System</h2>
+<header>
+    <h2>Arduino Booking System</h2>
+    <p><? if($db){
+        echo "Successful connection";
+    } else{
+        echo "Failed to connect to the database";
+    }
+    ?>
+    </p>
+</header>
 
-<? if($db){
-    echo "Successful connection";
-} else{
-    echo "Failed to connect to the database";
-}
-?>
+<main>
+
+
+<nav>
+    <?php
+    $sql_query = "SELECT * FROM category";
+    $result =  $db->query($sql_query);
+    if(mysqli_num_rows($result)>0){
+    $counter = 0;
+    while ($row = $result->fetch_array())
+    {
+    $counter++;
+    ?>
+<ul>
+    <li><a href="category.php?categoryName=<?php echo $row['categoryID'];?>"><?php echo $row['categoryName'];?></a></li>
+</ul>
+        <?php
+    }
+    }
+    $result->close();
+    $db->close();
+    ?>
+
+</nav>
+</main>
 
 </body>
 </html>
