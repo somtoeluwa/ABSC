4c0b287890fff58760d602347b6d0c373b7264ae
somtoeluwa <1412632@rgu.ac.uk>
2016-07-26 18:05:41 +0100

    Adding item details

--- dev/newItem.php
+++ dev/newItem.php
@@ -207,7 +207,7 @@ $category = $_GET['categoryID'];
 
 
                 $sql = "insert into asset (assetName,assetType,assetDescription,quantity,categoryID,serial_number,condition)
-                values('{$asset_Name}','{$asset_Type}','{$asset_Description}','{$quantity}','{$cat_ID}','{$serialnumber}', '{$condition}')";
+                values('$asset_Name','$asset_Type','$asset_Description','$quantity','$cat_ID','$serialnumber', '$condition')";
 
                     if($result = mysqli_query($db,$sql)){
                         // When sucessful return to blog.php(Show all blog entries)
