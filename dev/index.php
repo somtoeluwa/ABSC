37a95b33279e9b09b4fd60abc9c3f9fbc964bf41
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-07-27 19:30:59 +0100

    testing new image upload

--- dev/index.php
+++ dev/index.php
@@ -13,14 +13,13 @@ if($_SERVER['REQUEST_METHOD']==='GET'){
     }*/
 }
 else if($_SERVER['REQUEST_METHOD']==='POST'){	//Post is used when the form is submitted
-    //read input details from adminviewitems.php
+    //read input details from index.php
     $email=$_POST['u'];
     $password=$_POST['p'];
     $username = "";
     if(user_registered($email,$password)){	//See function below
         session_start();	//start the session
-        $_SESSION["ad_email"]=$email;
-        $_SESSION['ad_firstname'] = "Somto";//assign the admin email address to the session
+        $_SESSION["ad_email"]=$email;//assign the admin email address to the session
 
         header("Location: home.php");	//send admin to adminhome.php
     }
@@ -56,47 +55,57 @@ function user_registered($email,$password) {
     }
 }
 ?>
-<!--else{
-        echo "Impossible is nothing";
-    }
-function show_index_page() {
-//display the HTML form to register
-//or sign a user in
-$htmlpage = <<<HTMLPAGE-->
+
 
 <!DOCTYPE html>
 <html >
 <head>
     <meta charset="UTF-8">
     <title> Admin </title>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/loginpage.css">
+    <link rel="stylesheet" href="http://www.w3schools.com/lib/w3.css">
+    <link rel="stylesheet" href="css/w3.css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="http://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
 </head>
 
-<header>
-    <img id="logo" src="assets/images/logo.png" alt="Home logo">
-    <h3 id="title">Arduino component booking system</h3>
+<body class="w3-container">
+<header class="w3-container">
+    <h1 class="login_title"></h1>
 </header>
+<main>
+    <!-- Login form -->
+    <div id=loginform class="w3-modal-content w3-card-8 " style="max-width:450px; margin-top:100px; ">
+        <div class="w3-center">
+            <br>
+            <img src="assets/images/Robert_Gordon_University_logo.svg.png" alt="Avatar" style="width:60%" class=" w3-margin-top">
+        </div>
 
-<body>
-<h2 style="color: ghostwhite" align="center"> Please Login </h2>
-<div class="login-page">
-    <div class="form">
-        <form name="userlogin" method="post" action="index.php" >
-            <input type="text" name="u" placeholder="Username" required/>
-            <br><br>
-            <input type="password" name="p" placeholder="Password" required/>
-            <br>
-            <br>
-            <div class="submit">
-                <button type="submit" name="submit" value="login" ><Strong> Login </Strong> </button>
-                <br>
-                <br>
+        <form name="userlogin" method="post" action="index.php" class="w3-container">
+            <div class="w3-section">
+                <label><b>E-mail</b></label>
+                <input type="email" name="u" placeholder="Enter Email..."  class=" w3-input w3-border w3-round-large" required/>
+
+                <label><b>Password</b></label>
+                <input type="password" name="p" placeholder="Enter Password..."  class=" w3-input w3-border w3-round-large" required/>
+
+                <button type="submit" name="submit" class="w3-btn-block w3-section w3-purple w3-round-xxlarge" value="login"> Log in </button>
+                <input class="w3-check w3-margin-top" type="checkbox" checked="checked"> Remember me
             </div>
         </form>
+
+        <div class="w3-container w3-border-top w3-padding-16 w3-light-grey">
+            <span class="w3-right w3-padding w3-hide-small">Forgot <a href="#">password?</a></span>
+        </div>
+
     </div>
-</div>
+</main>
+
+<footer class= "w3-container w3-padding-4 w3-margin"  style="text-align: center; font-size:smaller; ">
+    <h11> Arduino Component Booking System. Copyright 2016</h11>
+</footer>
+
+</body>
+</html>
