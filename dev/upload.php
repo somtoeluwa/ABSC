ae100cd7a1fb4f250f8231e37ad2a7645ce2a2f4
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-08-05 22:52:20 +0100

    Version 3.0 from  local disk
    Admin functions
    - create item
    - upload item picture
    
    Customer functions
    - add to cart
    - remove from cart
    - check out
    
    .....Bugg detection phase ...
    -upload bug

--- dev/upload.php
+++ dev/upload.php
@@ -35,7 +35,7 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
 }
 
 // Allow certain file formats
-if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+/*if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
     && $imageFileType != "gif" ) {
     echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
@@ -46,7 +46,7 @@ if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
 
 // if everything is ok, try to upload file and create new item
-} else {
+}*/ else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
 // Simple function to avoid SQL Injection.
         function test_input($data) {
