c95d87e7a075099564e3779784a5df0ad1fd53ad
somtoeluwa <s.eluwa@rgu.ac.uk>
2016-08-08 23:20:09 +0100

    User management
    new login system and priviledges

--- dev/home.php
+++ dev/home.php
@@ -9,6 +9,7 @@
 // Start session
 session_start();
 $role = $_SESSION['sess_userrole'];
+$userid = $_SESSION['sess_user_id'];
 if(!isset($_SESSION['sess_email']) && ($role!="user"|| $role!="admin")){
     header('Location: index.php?err=2');
 }
@@ -59,7 +60,7 @@ $page_title ="Arduino component booking system";
             <li class="w3-dropdown-hover w3-right" id="profile">
                 <a class="w3-hover-purple" href="#"><i class="fa fa-user w3-large" aria-hidden="true"></i><i class="fa fa-caret-down"></i></a>
                 <div class="w3-dropdown-content w3-white w3-card-2">
-                    <a href="account.php?userid=<?php echo $_SESSION['sess_user_id'];?>"><?php echo $_SESSION['sess_firstname'];?>'s Profile</a>
+                    <a href="account.php?userid=<?php echo $userid ?>"><?php echo $_SESSION['sess_firstname'];?>'s Profile</a>
                     <?php
                     if ($role == "admin" ) {
                         ?>
